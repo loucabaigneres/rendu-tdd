613b8987caee333fb8f8888dde000e3ab1b821a8
Louca Baignères <baigneres.louca@gmail.com>
2025-10-17 16:56:15 +0200

    test: add test for adding a product to the cart

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -4,3 +4,9 @@ test('empty cart total is 0.00', () => {
   const cart = new Cart();
   expect(cart.total()).toBe(0.00);
 });
+
+test('adding one product returns its price', () => {
+  const cart = new Cart();
+  cart.addProduct('Socks', 1000); // 1000 centimes = 10.00 €
+  expect(cart.total()).toBe(10.00);
+});
